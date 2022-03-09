0dfbd327441916c82eab4451568ccc3ba038cd16
jacklynch00 <jrlynch00@gmail.com>
2022-03-09 16:59:37 -0500

    Added ratings method

--- Desert.Python.Api/Controllers/CatalogController.cs
+++ Desert.Python.Api/Controllers/CatalogController.cs
@@ -46,9 +46,15 @@ namespace Desert.Python.Api.Controllers
 		[HttpPost("{id:int}/ratings")]
 		public IActionResult PostRating(int id, [FromBody] Rating rating)
 		{
-			var item = new Item("Shirt", "Ohio State shirt", "Nike", 29.99m);
-			item.Id = id;
+			var item = _db.Items.Find(id);
+
+			if (item == null)
+			{
+				return NotFound();
+			}
+
 			item.AddRating(rating);
+			_db.SaveChanges();
 
 			return Ok(item);
 		}
