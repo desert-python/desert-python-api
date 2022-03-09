339205412bb7450753e80266ecce06c2479d9b1d
jacklynch00 <jrlynch00@gmail.com>
2022-03-09 16:07:52 -0500

    added store context to Controller

--- Desert.Python.Api/Controllers/CatalogController.cs
+++ Desert.Python.Api/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Desert.Python.Domain.catalog;
+using Desert.Python.Data;
 
 namespace Desert.Python.Api.Controllers
 {
@@ -7,16 +8,18 @@ namespace Desert.Python.Api.Controllers
 	[Route("[controller]")]
 	public class CatalogController : ControllerBase
 	{
+
+		private readonly StoreContext _db;
+
+		public CatalogController(StoreContext db)
+		{
+			_db = db;
+		}
+
 		[HttpGet]
 		public IActionResult GetItems()
 		{
-			var items = new List<Item>()
-			{
-				new Item("Shirt", "Ohio State shirt", "Nike", 29.99m),
-				new Item("Shirt", "Ohio State shirt", "Nike", 29.99m)
-			};
-
-			return Ok(items);
+			return Ok(_db.Items);
 		}
 
 		[HttpGet("{id:int}")]
--- Desert.Python.Api/Program.cs
+++ Desert.Python.Api/Program.cs
@@ -1,9 +1,16 @@
+using Desert.Python.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreContext>(options => 
+	options.UseSqlite("Data Source=../Registrar.sqlite",
+	b => b.MigrationsAssembly("Desert.Python.Api"))
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
