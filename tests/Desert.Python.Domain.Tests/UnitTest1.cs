14bd6cff5e214efa7d2dd3bb7005dd5a570eeb11
jacklynch00 <jrlynch00@gmail.com>
2022-04-05 18:35:09 -0400

    adding tests

---
+++ tests/Desert.Python.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Desert.Python.Domain.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
